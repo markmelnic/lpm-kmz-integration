1f3be93d9b02b6a7965de2c2d2e1d5a913d23f59
Mark Melnic <markhappy200@gmail.com>
2020-12-28 13:14:42 +0200

    Adjusted pollution levels

--- lpm/settings.py
+++ lpm/settings.py
@@ -9,10 +9,10 @@ COLORS = [
     (0, 150, 210),    # 7 light blue
     (0, 75, 180),     # 8 blue
     (0, 35, 115),     # 9 dark blue
-    #(128, 128, 128),  # 10 light grey
-    #(77, 77, 77),     # 11 dark grey
+    (128, 128, 128),  # 10 light grey
+    (77, 77, 77),     # 11 dark grey
     #(0, 0, 0),        # 12 black
 ]
 
-HI_P = COLORS[:6]
-LO_P = COLORS[6:10] # skipped black since it's usually ocean
+HI_P = COLORS[:7]
+LO_P = COLORS[7:10] # skipped black since it's usually ocean
